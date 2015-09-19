fa2c3f23fc680a70a7c3900d473e518b5d5fc0b5
Andre Ramaciotti <andre@ramaciotti.com>
2015-09-19 08:30:57 -0300

    link zshrc after installing oh-my-zsh

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,17 +5,16 @@
 
 ${DOT_FILES:=~/dot-files}
 
-# Create symlinks
-ln -s $DOT_FILES/zshrc $HOME/.zshrc
-ln -s $DOT_FILES/vim $HOME/.vim
-ln -s $DOT_FILES/vimrc $HOME/.vimrc
-
 # Download zsh and its syntax-highlighting plugin
 zsh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone git://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins
+rm $HOME/.zshrc
+ln -s $DOT_FILES/zshrc $HOME/.zshrc
 
 # Download vim-plug and install plugins
-curl -fLo /home/andre/.vim/autoload/plug.vim --create-dirs \
+ln -s $DOT_FILES/vim $HOME/.vim
+ln -s $DOT_FILES/vimrc $HOME/.vimrc
+curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs \
 	https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 vim -c PlugInstall -c q -c q
 
