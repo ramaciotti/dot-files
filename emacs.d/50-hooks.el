7818053215a6cf274bd1a8bfea8f4abc68a9dcb8
Andre Ramaciotti <andre@ramaciotti.com>
2016-10-11 19:06:15 -0300

    Add Elixir and Typescript modes to Emacs

--- emacs.d/50-hooks.el
+++ emacs.d/50-hooks.el
@@ -1,4 +1,13 @@
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
-(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'tuareg-mode-hook 'merlin-mode)
+
+(add-hook 'c-mode-common-hook
+          (lambda ()
+            (setq c-basic-offset 4)
+            (c-set-offset 'substatement-open 0)))
+
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+
 (add-hook 'caml-mode-hook 'merlin-mode)
+(add-hook 'tuareg-mode-hook 'merlin-mode)
+
+(add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
