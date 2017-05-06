ded5d632bfeff823279876f6a90cff21181676fa
Andre Ramaciotti <andre@ramaciotti.com>
2017-05-06 12:05:23 -0300

    Replace xterm with urxvt

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -43,7 +43,7 @@ end
 beautiful.init(awful.util.get_themes_dir() .. "default/theme.lua")
 
 -- This is used later as the default terminal and editor to run.
-terminal = "xterm"
+terminal = "urxvt"
 editor = os.getenv("EDITOR") or "nano"
 editor_cmd = terminal .. " -e " .. editor
 
